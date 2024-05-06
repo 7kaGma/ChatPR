7a47b70ae7d26305d48ecabac797f225a79e3f35
7kaGma <trlm7ka2ma80@gmail.com>
2024-05-06 21:58:08 +0900

    データの登録

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,25 +1,42 @@
-// /*=========
-// Firebaseの設定
-// ==========*/
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-// // RealtimeDatabase
-// import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved, onValue  } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
-// // Autentication
+/*=========
+Firebaseの設定
+==========*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+// RealtimeDatabase
+import { getDatabase, ref, push, set, onChildAdded, remove, onChildRemoved, onValue  } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+// Autentication
 
 
-// // Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD084xtONTFcO5v3_bY6H4jtMVlBaGkyuQ",
-//   authDomain: "chatpr-f9896.firebaseapp.com",
-//   projectId: "chatpr-f9896",
-//   storageBucket: "chatpr-f9896.appspot.com",
-//   messagingSenderId: "132773649127",
-//   appId: "1:132773649127:web:76f35dd55dd8af47b5e042"
-// };
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD084xtONTFcO5v3_bY6H4jtMVlBaGkyuQ",
+  authDomain: "chatpr-f9896.firebaseapp.com",
+  projectId: "chatpr-f9896",
+  storageBucket: "chatpr-f9896.appspot.com",
+  messagingSenderId: "132773649127",
+  appId: "1:132773649127:web:76f35dd55dd8af47b5e042"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// /*==========
-// Database
-// ==========*/
+/*==========
+Database
+==========*/
+const db = getDatabase(app);
+const dbRef = ref(db, "chat");
+
+/*Databaseへの登録*/
+$("#submit").on("click",function(){
+  let time = Date.now();
+  let today = Date(time);
+
+  const post = {
+    text:$("#text").val(),
+    time:today
+  }  
+  
+  const newPostRef= push(dbRef);
+  set(newPostRef,post);
+
+});
